2267a86d807dbde73bd70b8b420192463462dad2
Shima957 <81807747+Shima957@users.noreply.github.com>
2022-12-17 20:50:20 +0900

    feat: Feature/typography (#2)
    * initial-commit
    
    * test
    
    * feat: add Typography Component

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   srcDir: "src/",
+  components: [
+    {
+      path: "@/components/",
+      pathPrefix: false,
+    },
+  ],
 });
