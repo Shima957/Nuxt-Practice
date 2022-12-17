1a23025dc14a02aa0f669e47c90f57e63b024b63
Shima957 <shimasound04@gmail.com>
2022-12-17 11:46:04 +0000

    feat: add Typography Component

---
+++ src/components/ui/Typography.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+  const TypographyVariants = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'span'] as const
+  type TypographyVariantsType = typeof TypographyVariants[number]
+  type Props = {
+    variants?: TypographyVariantsType
+  }
+
+  const prop = withDefaults(defineProps<Props>(), {
+    variants: 'span'
+  })
+</script>
+
+<template>
+  <component :is="variants">
+    <slot />
+  </component>
+</template>
