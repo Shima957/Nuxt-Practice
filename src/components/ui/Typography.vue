c899743ea73358a0ac3f8960d6b83f93a97df87d
Shima957 <shimasound04@gmail.com>
2022-12-19 22:01:07 +0900

    feat: add style props

--- src/components/ui/Typography.vue
+++ src/components/ui/Typography.vue
@@ -2,16 +2,35 @@
   const TypographyVariants = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'span'] as const
   type TypographyVariantsType = typeof TypographyVariants[number]
   type Props = {
-    variants?: TypographyVariantsType
+    variants?: TypographyVariantsType;
+    center?: boolean;
+    bold?:boolean;
   }
 
-  const prop = withDefaults(defineProps<Props>(), {
-    variants: 'span'
+  const props = withDefaults(defineProps<Props>(), {
+    variants: 'span',
+  })
+
+  const classnames = computed(() => {
+    return {
+      'center': props.center,
+      'bold': props.bold
+    }
   })
 </script>
 
 <template>
-  <component :is="variants">
+  <component :is="variants" :class="classnames">
     <slot />
   </component>
 </template>
+
+<style lang="scss">
+.center {
+  text-align: center;
+}
+
+.bold {
+  font-weight: bold;
+}
+</style>
