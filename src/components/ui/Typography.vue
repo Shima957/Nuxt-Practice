036fc3fe647ede01445f32b5e6c28be506a09b61
Shima957 <shimasound04@gmail.com>
2022-12-24 13:25:51 +0900

    feat: add typograhy styles

--- src/components/ui/Typography.vue
+++ src/components/ui/Typography.vue
@@ -10,17 +10,45 @@
   const props = withDefaults(defineProps<Props>(), {
     variants: 'span',
   })
-
+ 
   const classnames = computed(() => {
     return {
       'center': props.center,
       'bold': props.bold
     }
   })
+
+  const variantStyle = computed(() => {
+    switch(props.variants) {
+      case 'h1':
+        return 'heading-one'
+
+      case 'h2':
+        return 'heading-two'
+
+      case 'h3':
+        return 'heading-three'
+
+      case 'h4':
+        return 'subtitle'
+
+      case 'h5':
+        return 'subtitle'
+
+      case 'h6':
+        return 'subtitle'
+
+      case 'p':
+        return 'body'
+
+      default:
+        return 'normal'
+    }
+  })
 </script>
 
 <template>
-  <component :is="variants" :class="classnames">
+  <component :is="variants" :class="[classnames, variantStyle]">
     <slot />
   </component>
 </template>
@@ -32,5 +60,29 @@
 
 .bold {
   font-weight: bold;
+}
+
+.heading-one {
+  font-size: 51px;
+}
+
+.heading-two {
+  font-size: 30px;
+}
+
+.heading-three {
+  font-size: 24px;
+}
+
+.subtitle {
+  font-size: 20px;
+}
+
+.body {
+  font-size: 14px;
+}
+
+.normal {
+  font-size: 12px;
 }
 </style>
